234b08126dd4e63a7fbac27a8c0ccc8752cccdbf
Gavin Simpson <ucfagls@gmail.com>
2011-06-29 07:28:26 +0000

    rip out the in-line functions that do the permutation and make available as top-level functions. Document.

---
+++ R/shuffle-utils.R
@@ -0,0 +1,72 @@
+
+`shuffleStrata` <- function(strata, type, mirror = FALSE, start = NULL,
+                            flip = NULL, nrow, ncol, start.row = NULL,
+                            start.col = NULL) {
+    lev <- length(levels(strata))
+    ngr <- length(strata) / lev
+    sp <- split(seq(along = strata), strata)
+    if(type == "free") {
+        unname(do.call(c, sp[shuffleFree(lev, lev)]))
+    } else if(type == "series") {
+        unname(do.call(c,
+                       sp[shuffleSeries(seq_len(lev),
+                                        mirror = mirror,
+                                        start = start,
+                                        flip = flip)]))
+    } else if(type == "grid") {
+        unname(do.call(c,
+                       sp[shuffleGrid(nrow = nrow, ncol = ncol,
+                                      mirror = mirror,
+                                      start.row = start.row,
+                                      start.col = start.col,
+                                      flip = flip)]))
+    } else {
+        stop("Invalid permutation type.")
+    }
+}
+
+`shuffleGrid` <- function(nrow, ncol, mirror = FALSE, start.row = NULL,
+                          start.col = NULL, flip = NULL) {
+    if(is.null(start.row))
+        start.row <- shuffleFree(nrow, 1L)
+    if(is.null(start.col))
+        start.col <- shuffleFree(ncol, 1L)
+    ir <- seq(start.row, length=nrow) %% nrow
+    ic <- seq(start.col, length=ncol) %% ncol
+    if(!is.null(flip)) {
+        if(any(flip)) {
+            if(flip[1L])
+                ir <- rev(ir)
+            if(flip[2L])
+                ic <- rev(ic)
+        }
+    } else {
+        if (mirror) {
+            if (runif(1L) < 0.5)
+                ir <- rev(ir)
+            if (runif(1L) < 0.5)
+                ic <- rev(ic)
+        }
+    }
+    rep(ic, each=nrow) * nrow + rep(ir, len=nrow*ncol) + 1L
+}
+
+`shuffleSeries` <- function(x, mirror = FALSE, start = NULL,
+                            flip = NULL) {
+    n <- length(x)
+    if(is.null(start))
+        start <- shuffleFree(n, 1L)
+    out <- seq(start, length = n) %% n + 1L
+    if(!is.null(flip)) {
+        if(flip)
+            out <- rev(out)
+    } else {
+        if(mirror && runif(1L) < 0.5)
+            out <- rev(out)
+    }
+    x[out]
+}
+
+`shuffleFree` <- function(x, size) {
+    .Internal(sample(x, size, FALSE, NULL))
+}
--- R/shuffle.R
+++ R/shuffle.R
@@ -1,86 +1,16 @@
 `shuffle` <-
     function (n, control = permControl())
 {
-    `pStrata` <- function(strata, type, mirror = FALSE, start = NULL,
-                          flip = NULL, nrow, ncol, start.row = NULL,
-                          start.col = NULL) {
-            lev <- length(levels(strata))
-            ngr <- length(strata) / lev
-            sp <- split(seq(along = strata), strata)
-            if(type == "free") {
-                unname(do.call(c, sp[pFree(lev, lev)]))
-            } else if(type == "series") {
-                unname(do.call(c,
-                               sp[pSeries(seq_len(lev),
-                                          mirror = mirror,
-                                          start = start,
-                                          flip = flip)]))
-            } else if(type == "grid") {
-                unname(do.call(c,
-                               sp[pGrid(nrow = nrow, ncol = ncol,
-                                        mirror = mirror,
-                                        start.row = start.row,
-                                        start.col = start.col,
-                                        flip = flip)]))
-            } else {
-                stop("Invalid permutation type.")
-            }
-        }
-    `pGrid` <- function(nrow, ncol, mirror = FALSE, start.row = NULL,
-                        start.col = NULL, flip = NULL) {
-            if(is.null(start.row))
-                start.row <- pFree(nrow, 1L)
-            if(is.null(start.col))
-                start.col <- pFree(ncol, 1L)
-            ir <- seq(start.row, length=nrow) %% nrow
-            ic <- seq(start.col, length=ncol) %% ncol
-            if(!is.null(flip)) {
-                if(any(flip)) {
-                    if(flip[1L])
-                        ir <- rev(ir)
-                    if(flip[2L])
-                        ic <- rev(ic)
-                }
-            } else {
-                if (mirror) {
-                    if (runif(1L) < 0.5)
-                        ir <- rev(ir)
-                    if (runif(1L) < 0.5)
-                        ic <- rev(ic)
-                }
-            }
-            rep(ic, each=nrow) * nrow + rep(ir, len=nrow*ncol) + 1L
-        }
-    `pSeries` <- function(inds, mirror = FALSE, start = NULL,
-                          flip = NULL) {
-        n <- length(inds)
-        if(is.null(start))
-            start <- pFree(n, 1L)
-        out <- seq(start, length = n) %% n + 1L
-        if(!is.null(flip)) {
-            if(flip)
-                out <- rev(out)
-        } else {
-            if(mirror && runif(1L) < 0.5)
-                out <- rev(out)
-        }
-        inds[out]
-    }
-    `pFree` <- function(x, size)
-        .Internal(sample(x, size, FALSE, NULL))
-    ## END in-line Functions ##
-
     ## If no strata then permute all samples using stated scheme
     if(is.null(control$strata)) {
         out <-
             switch(control$within$type,
-                   "free" = pFree(n, n),
-                   "series" =
-                   pSeries(1:n, mirror = control$within$mirror),
-                   "grid" =
-                   pGrid(nrow = control$within$nrow,
-                         ncol = control$within$ncol,
-                         mirror = control$within$mirror),
+                   "free" = shuffleFree(n, n),
+                   "series" = shuffleSeries(1:n,
+                   mirror = control$within$mirror),
+                   "grid" = shuffleGrid(nrow = control$within$nrow,
+                   ncol = control$within$ncol,
+                   mirror = control$within$mirror),
                    "none" = seq_len(n)
                    )
     } else {
@@ -91,12 +21,12 @@
             out <- 1:n
         } else {
             flip <- runif(1L) < 0.5
-            out <- pStrata(control$strata,
-                           type = control$blocks$type,
-                           mirror = control$blocks$mirror,
-                           flip = flip,
-                           nrow = control$blocks$nrow,
-                           ncol = control$blocks$ncol)
+            out <- shuffleStrata(control$strata,
+                                 type = control$blocks$type,
+                                 mirror = control$blocks$mirror,
+                                 flip = flip,
+                                 nrow = control$blocks$nrow,
+                                 ncol = control$blocks$ncol)
         }
         ## permute the samples within strata?
         if(control$within$type != "none") {
@@ -107,13 +37,13 @@
             if(control$within$constant) {
                 if(control$within$type == "free") {
                     n <- unique(tab)[1L]
-                    same.rand <- pFree(n, n)
+                    same.rand <- shuffleFree(n, n)
                 } else if(control$within$type == "series") {
-                    start <- pFree(n / length(inds), 1L)
+                    start <- shuffleFree(n / length(inds), 1L)
                     flip <- runif(1L) < 0.5
                 } else if(control$within$type == "grid") {
-                    start.row <- pFree(control$within$nrow, 1L)
-                    start.col <- pFree(control$within$ncol, 1L)
+                    start.row <- shuffleFree(control$within$nrow, 1L)
+                    start.col <- shuffleFree(control$within$ncol, 1L)
                     flip <- runif(2L) < 0.5
                 }
             } else {
@@ -133,19 +63,18 @@
                                if(control$within$constant) {
                                    gr[same.rand]
                                } else {
-                                   out[gr][pFree(n.gr, n.gr)]
+                                   out[gr][shuffleFree(n.gr, n.gr)]
                                },
                                "series" =
-                               pSeries(gr,
-                                       mirror = control$within$mirror,
-                                       start = start, flip = flip),
+                               shuffleSeries(gr, mirror = control$within$mirror,
+                                             start = start, flip = flip),
                                "grid" =
-                               gr[pGrid(nrow = control$within$nrow,
-                                        ncol = control$within$ncol,
-                                        mirror = control$within$mirror,
-                                        start.row = start.row,
-                                        start.col = start.col,
-                                        flip = flip)]
+                               gr[shuffleGrid(nrow = control$within$nrow,
+                                              ncol = control$within$ncol,
+                                              mirror = control$within$mirror,
+                                              start.row = start.row,
+                                              start.col = start.col,
+                                              flip = flip)]
                                )
                 }
             }
