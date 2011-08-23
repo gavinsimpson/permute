61c4381cf656f543683ab3fb57bc233ca5ffd1cc
Jari Oksanen <jari.oksanen@oulu.fi>
2011-08-23 13:10:25 +0000

    make work with R < 2.13.0

--- R/nobs-methods.R
+++ R/nobs-methods.R
@@ -1,3 +1,7 @@
+## nobs generic is defined only in R 2.13.0: define here for older R
+if (getRversion() < "2.13.0")
+    nobs <- function(object, ...) UseMethod("nobs")
+
 ## add some nobs() methods - need to be documented
 `nobs.numeric` <- function(object, ...) {
     length(object)
