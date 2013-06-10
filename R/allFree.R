771e9bdca91e4436d6988162931bf181c8816e81
Gavin Simpson <ucfagls@gmail.com>
2013-06-10 20:01:53 +0000

    clean up

--- R/allFree.R
+++ R/allFree.R
@@ -1,15 +1,3 @@
-## `allFree` <- function(n, v = 1:n)
-## {
-##     if( n == 1 ) {
-##         matrix(v, 1, 1)
-##     } else {
-##         X <- NULL
-##         for(i in 1:n)
-##             X <- rbind(X, cbind(v[i], Recall(n-1, v[-i])))
-##         X
-##     }
-## }
-
 ## Modified version of allFree() provided by Doug Bates
 ## via personal email on 19 Jan 2012
 `allFree` <- function(n, v = seq_len(n)) {
