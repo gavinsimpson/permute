9f61bca3dbcf133e4e2a1d3c5e28a3a0c4731ae3
Gavin Simpson <ucfagls@gmail.com>
2013-11-21 16:30:03 +0000

    add fixupCall(), a new function to programmatically update the call stored in the how object.

---
+++ R/fixupCall.R
@@ -0,0 +1,12 @@
+## Non-exported function that can be used to fix up elements of a call
+`fixupCall` <- function(object, element, value) {
+    ## element must be character! Try to coerce
+    element <- as.character(element)     ## Fails if not coercible
+    .call <- getElement(object, "call")  ## get call from object
+    .ll <- as.list(.call)                ## convert to a list to manipulate
+    names(.ll) <- names(.call)           ## set names on the list
+    .ll[[element]] <- value              ## set element to value
+    .call <- as.call(.ll)                ## convert back to a call
+    object$call <- .call                 ## push back into object
+    object
+}
