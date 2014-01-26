4f5ad16f05c12b9fb21a5e526ae9a8e07fe0f798
Gavin Simpson <ucfagls@gmail.com>
2014-01-26 02:18:45 +0000

    getting the block indices to vary slowest with first block to fastest for last block needed rethinking. Fix allows this to work for even and uneven sized blocks.

--- R/cbindAllPerms.R
+++ R/cbindAllPerms.R
@@ -6,17 +6,20 @@
 `cbindAllPerms` <- function(x) {
     nb <- length(x) ## number of blocks
 
-    ## prepares nb seqence vectors 1:`obs in block` for expand.grid
-    rowind <- do.call(expand.grid, lapply(x, function(i) seq_len(nrow(i))))
+    ## allPerms has first block varying slowest, but expand.grid has
+    ## first factor varying fastest. Hence we reverse `x` here, and
+    ## also reverse `out` back again later
+    x <- rev(x)
 
-    ## contains row indices for each block, but 1st block varies fastest
-    ## and allPerms() traditionally had nth block varying fastest, so
-    ## reverse order of columns. drop ensures this work if only 1 block.
-    rowind <- rowind[, seq.int(nb, 1), drop = FALSE]
+    ## prepares nb seqence vectors 1:`obs in block` for expand.grid
+    rowind <- do.call(expand.grid,
+                      lapply(x, function(i) seq_len(nrow(i))))
 
     ## index elements of x using the row indices - gives a list to cbind
-    ## next. sapply() over-simplifies to wrong dimensions so not used
-    out <- lapply(seq_len(nb), function(i, m, ind) m[[i]][ind[, i] ,],
-                  m = x, ind = rowind)
-    do.call(cbind, out) ## returns
+    ## next. sapply() over-simplifies to wrong dimensions so not used.
+    ## Note: the lapply() result is reversed with `rev` to undo the reversing
+    ## of `x` earlier; ensures everything is correct block order.
+    out <- rev(lapply(seq_len(nb), function(i, m, ind) m[[i]][ind[, i] ,],
+                      m = x, ind = rowind))
+    do.call(cbind, out) ## return
 }
