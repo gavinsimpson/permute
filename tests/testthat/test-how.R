59bd11d3ce464de8c124cbb95e89af29428c507a
Gavin Simpson <ucfagls@gmail.com>
2015-05-26 15:22:09 -0600

    add a new test of how() that it works with explicit NULL blocks as per #8

---
+++ tests/testthat/test-how.R
@@ -0,0 +1,10 @@
+library("testthat")
+library("permute")
+
+context("Testing how()")
+
+test_that("how() works with explicit NULL blocks arg", {
+    ## Example of failure from Jari github #8
+    h <- how(blocks = NULL)
+    expect_that(h, is_a("how"))
+})
