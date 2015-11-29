dd5ce754d1f5ce9f17117951eb876a2121bd74ad
Gavin Simpson <ucfagls@gmail.com>
2015-11-29 11:44:23 -0600

    fixupCall failed if element of cal didn't already exist

--- R/fixupCall.R
+++ R/fixupCall.R
@@ -5,7 +5,15 @@
     .call <- getElement(object, "call")  ## get call from object
     .ll <- as.list(.call)                ## convert to a list to manipulate
     names(.ll) <- names(.call)           ## set names on the list
-    .ll[[element]] <- value              ## set element to value
+    ## if element not in names need to create it
+    if (! element %in% names(.ll)) {
+        new <- list(value)
+        names(new) <- element
+        .ll <- c(.ll, new)
+    } else {
+        ## otherwise just update it
+        .ll[[element]] <- value              ## set element to value
+    }
     .call <- as.call(.ll)                ## convert back to a call
     object$call <- .call                 ## push back into object
     object
