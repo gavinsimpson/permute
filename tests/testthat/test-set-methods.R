413a0e62264ec1226b6a6be8d04e385db16d69aa
Gavin Simpson <ucfagls@gmail.com>
2015-11-28 19:12:09 -0600

    new tests for set<- methods

---
+++ tests/testthat/test-set-methods.R
@@ -0,0 +1,32 @@
+library("testthat")
+library("permute")
+
+context("Testing set methods")
+
+test_that("default methods for get functions", {
+    v <- 1:10
+    expect_error(setNperm(v) <- 10, regexp = "No default method")
+    expect_error(setMaxperm(v) <- 10, regexp = "No default method")
+    expect_error(setMinperm(v) <- 10, regexp = "No default method")
+    expect_error(setComplete(v) <- TRUE, regexp = "No default method")
+    expect_error(setBlocks(v) <- TRUE, regexp = "No default method")
+    expect_error(setAllperms(v) <- 1:10, regexp = "No default method")
+    expect_error(setObserved(v) <- TRUE, regexp = "No default method")
+    expect_error(setPlots(v) <- Plots(), regexp = "No default method")
+    expect_error(setWithin(v) <- Within(), regexp = "No default method")
+    expect_error(setStrata(v) <- gl(2, 5), regexp = "No default method")
+    expect_error(setRow(v) <- 4, regexp = "No default method")
+    expect_error(setCol(v) <- 5, regexp = "No default method")
+    expect_error(setDim(v) <- c(2,3), regexp = "No default method")
+    expect_error(setType(v) <- "series", regexp = "No default method")
+    expect_error(setMirror(v) <- TRUE, regexp = "No default method")
+    expect_error(setConstant(v) <- "series", regexp = "No default method")
+
+    h <- how()
+    expect_error(setDim(h) <- c(2,3), regexp = "can not be used directly on '\"how\"' objects")
+    expect_error(setMirror(h) <- TRUE, regexp = "can not be used directly on '\"how\"' objects")
+    expect_error(setConstant(h) <- TRUE, regexp = "can not be used directly on '\"how\"' objects")
+    expect_error(setType(h) <- "series", regexp = "can not be used directly on '\"how\"' objects")
+    expect_error(setRow(h) <- 2, regexp = "can not be used directly on '\"how\"' objects")
+    expect_error(setCol(h) <- 3, regexp = "can not be used directly on '\"how\"' objects")
+})
