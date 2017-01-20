5bae7d866a4e064ac2c524630da78e4693063705
Gavin Simpson <ucfagls@gmail.com>
2017-01-19 21:50:40 -0600

    fix indentation

--- R/numPerms.R
+++ R/numPerms.R
@@ -1,104 +1,105 @@
 `numPerms` <- function(object, control = how()) {
-  ## constant holding types where something is permuted
-  TYPES <- c("free","grid","series","none")
+    ## constant holding types where something is permuted
+    TYPES <- c("free","grid","series","none")
 
-  ## expand object if a numeric or integer vector of length 1
-  if((is.numeric(object) || is.integer(object)) &&
-     (length(object) == 1))
-    object <- seq_len(object)
-  ## number of observations in data
-  n <- nobs(object)
+    ## expand object if a numeric or integer vector of length 1
+    if((is.numeric(object) || is.integer(object)) &&
+       (length(object) == 1))
+        object <- seq_len(object)
+    ## number of observations in data
+    n <- nobs(object)
 
-  ## get the permutation levels from control
-  WI <- getWithin(control)
-  PL <- getPlots(control)
-  BL <- getBlocks(control)
+    ## get the permutation levels from control
+    WI <- getWithin(control)
+    PL <- getPlots(control)
+    BL <- getBlocks(control)
 
-  ## any strata to permute within / blocking?
-  BLOCKS <- getStrata(control, which = "blocks")
-  PSTRATA <- getStrata(control, which = "plots")
-  typeP <- getType(control, which = "plots")
-  typeW <- getType(control, which = "within")
+    ## any strata to permute within / blocking?
+    BLOCKS <- getStrata(control, which = "blocks")
+    PSTRATA <- getStrata(control, which = "plots")
+    typeP <- getType(control, which = "plots")
+    typeW <- getType(control, which = "within")
 
-  ## mirroring?
-  mirrorP <- getMirror(control, which = "plots")
-  mirrorW <- getMirror(control, which = "within")
+    ## mirroring?
+    mirrorP <- getMirror(control, which = "plots")
+    mirrorW <- getMirror(control, which = "within")
 
-  ## constant - i.e. same perm within each plot?
-  constantW <- getConstant(control)
+    ## constant - i.e. same perm within each plot?
+    constantW <- getConstant(control)
 
-  ## grid dimensions
-  colW <- getCol(control, which = "within")
-  colP <- getRow(control, which = "plots")
+    ## grid dimensions
+    colW <- getCol(control, which = "within")
+    colP <- getRow(control, which = "plots")
 
-  ## Some checks; i) Plot strata must be of same size when permuting strata
-  ##                 or having the same constant permutation within strata
-  ##             ii) In grid designs, grids must be of the same size for all
-  ##                 strata
-  ##
-  ## FIXME - this probably should be in check()!
-  if(!is.null(PSTRATA)) {
-    tab <- table(PSTRATA)
-    same.n <- length(unique(tab))
-    if((typeP != "none" || isTRUE(constantW)) && same.n > 1) {
-      stop("All levels of strata must have same number of samples for chosen scheme")
+    ## Some checks; i) Plot strata must be of same size when permuting strata
+    ##                 or having the same constant permutation within strata
+    ##             ii) In grid designs, grids must be of the same size for all
+    ##                 strata
+    ##
+    ## FIXME - this probably should be in check()!
+    if(!is.null(PSTRATA)) {
+        tab <- table(PSTRATA)
+        same.n <- length(unique(tab))
+        if((typeP != "none" || isTRUE(constantW)) && same.n > 1) {
+            stop("All levels of strata must have same number of samples for chosen scheme")
+        }
+        if(typeP == "grid" && same.n > 1) {
+            stop("Unbalanced grid designs are not supported")
+        }
     }
-    if(typeP == "grid" && same.n > 1) {
-      stop("Unbalanced grid designs are not supported")
+
+    ## the various designs allowed imply multipliers to number of samples
+    ## for the restricted permutations
+
+    mult.p <- mult.wi <- 1
+
+    ## within types
+    if(typeW %in% c("series","grid")) {
+        mult.wi <- 2
+        if(isTRUE(all.equal(typeW, "grid")) && !is.null(colW) && colW > 2) {
+            mult.wi <- 4
+        } else {
+            if(isTRUE(all.equal(n, 2)))
+                mult.wi <- 1
+        }
     }
-  }
-
-  ## the various designs allowed imply multipliers to number of samples
-  ## for the restricted permutations
-
-  mult.p <- mult.wi <- 1
-
-  ## within types
-  if(typeW %in% c("series","grid")) {
-    mult.wi <- 2
-    if(isTRUE(all.equal(typeW, "grid")) && !is.null(colW) && colW > 2) {
-      mult.wi <- 4
-    } else {
-      if(isTRUE(all.equal(n, 2)))
-        mult.wi <- 1
+    ## plot-level types
+    if(typeP %in% c("series","grid")) {
+        mult.p <- 2
+        if(isTRUE(all.equal(typeP, "grid")) && !is.null(colP) && colP > 2) {
+            mult.p <- 4
+        } else {
+            if(isTRUE(all.equal(length(tab), 2))) # was all.equal(n, 2)
+                mult.p <- 1
+        }
     }
-  }
-  ## plot-level types
-  if(typeP %in% c("series","grid")) {
-    mult.p <- 2
-    if(isTRUE(all.equal(typeP, "grid")) && !is.null(colP) && colP > 2) {
-      mult.p <- 4
-    } else {
-      if(isTRUE(all.equal(length(tab), 2))) # was all.equal(n, 2)
-        mult.p <- 1
+
+    ## within
+    ## another check - shouldn't this be moved? FIXME
+    if(!typeW %in% TYPES) {
+        stop("Ambiguous permutation type in 'control$within$type'")
     }
-  }
 
-  ## within
-  ## another check - shouldn't this be moved? FIXME
-  if(!typeW %in% TYPES) {
-    stop("Ambiguous permutation type in 'control$within$type'")
-  }
+    ## calculate the number of possible permutations
 
-  ## calculate the number of possible permutations
+    ## Compute number of permutations for each block
+    if(is.null(BLOCKS)) {
+        BLOCKS <- factor(rep(1, n))
+    }
 
-  ## Compute number of permutations for each block
-  if(is.null(BLOCKS))
-      BLOCKS <- factor(rep(1, n))
+    ## split an index vector
+    indv <- seq_len(n)
+    spl <- split(indv, BLOCKS)
 
-  ## split an index vector
-  indv <- seq_len(n)
-  spl <- split(indv, BLOCKS)
+    ## loop over the components of spl & apply doNumPerms
+    np <- sapply(spl, doNumPerms, mult.p, mult.wi, typeP, typeW, PSTRATA,
+                 mirrorP, mirrorW, constantW)
 
-  ## loop over the components of spl & apply doNumPerms
-  np <- sapply(spl, doNumPerms, mult.p, mult.wi, typeP, typeW, PSTRATA,
-               mirrorP, mirrorW, constantW)
+    ## multiply up n perms per block
+    np <- prod(np)
 
-  ## multiply up n perms per block
-  np <- prod(np)
-
-  class(np) <- "numPerms"
-  np
+    class(np) <- "numPerms"
+    np
 }
 
 `doNumPerms` <- function(obs, mult.p, mult.wi, typeP, typeW, PSTRATA,
